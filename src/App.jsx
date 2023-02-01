b0ea01b1633fe5546628286a0e4c244f9af58962
Antonio <antonio.webdesign19@gmail.com>
2023-02-01 19:28:22 +0200

    sc4

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="sm:h-screen bg-gray-900 poppins py-24 px-8 md:py-1 lg:py-24 h-auto">
+      <div className="bg-gray-900 poppins py-24 px-8 md:py-1 lg:py-24">
         <StartComponent />
       </div>
     </>
