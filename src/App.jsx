0111675e7dc9af76ab1319d5f5dd1d4f094a1466
Antonio <antonio.webdesign19@gmail.com>
2023-02-02 23:48:03 +0200

    vercel json

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <div className="bg-gray-900 poppins py-24 px-8 md:p-24 lg:p-32">
         <Routes>
-          <Route path="*" element={<StartComponent />} exact />
+          <Route path="/" element={<StartComponent />} exact />
           <Route path="/search" element={<SearchComponent />} />
         </Routes>
       </div>
--- src/components/StartComponent.jsx
+++ src/components/StartComponent.jsx
@@ -10,7 +10,7 @@ export default function StartComponent(props) {
       <div className="w-full flex justify-center my-4 md:w-6/12 mx-auto">
         <img src={illustration} className="object-cover"></img>
       </div>
-      <Link to="/search">
+      <Link to="/search" className="w-full flex justify-center">
         <button
           onClick={props.clicked}
           className="w-full text-xl text-gray-50 font-semibold bg-gradient-to-r from-myPurple to-yellow-200 py-4 rounded-3xl md:w-3/4 lg:w-2/4"
